bded4f3122800d47854991ca24a61b1bcccdcb25
Zach Schneider <schneidmaster@gmail.com>
2015-06-22 15:02:24 -0500

    Cleanup rubocop

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -26,7 +26,7 @@ Dummy::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  
+
   # Required for Rails 3.
   config.assets.enabled = true
   config.secret_token = 'k2114xt0GjwDi144950gO2er1619i06V'
