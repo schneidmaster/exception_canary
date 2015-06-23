d0aabf1e6691f82eeefb41e4d9a4d02501f16e65
Zach Schneider <schneidmaster@gmail.com>
2015-06-23 13:54:07 -0500

    Move rule before stacktrace, collapse stacktrace

--- app/assets/javascripts/exception_canary/application.js
+++ app/assets/javascripts/exception_canary/application.js
@@ -1,2 +1,10 @@
 //= require jquery
 //= require jquery_ujs
+
+$(document).on('ready', function() {
+  $('#show-more').on('click', function(event) {
+    $('.backtrace').css('height', 'auto');
+    $('#show-more').remove();
+    event.preventDefault();
+  });
+});
