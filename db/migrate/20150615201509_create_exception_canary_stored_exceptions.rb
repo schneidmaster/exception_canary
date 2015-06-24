cdf9a6ebeeecb9785758e1a4111684554abc91ba
Zach Schneider <schneidmaster@gmail.com>
2015-06-24 11:21:24 -0500

    Use longer fields to ensure exception model can hold data

--- db/migrate/20150615201509_create_exception_canary_stored_exceptions.rb
+++ db/migrate/20150615201509_create_exception_canary_stored_exceptions.rb
@@ -2,10 +2,10 @@ class CreateExceptionCanaryStoredExceptions < ActiveRecord::Migration
   def change
     create_table :exception_canary_stored_exceptions do |t|
       t.belongs_to :rule
-      t.string :title
-      t.string :backtrace
-      t.string :environment
-      t.string :variables
+      t.text :title
+      t.text :backtrace
+      t.text :environment
+      t.text :variables
       t.string :klass
       t.timestamps null: false
     end
